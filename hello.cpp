7ba964d3db85b9b968c54a7b79b0cc0428cab2af
brettmroach <brettmroach@ucsb.edu>
2020-01-08 23:16:31 -0800

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+// hello.cpp
+// Brett Roach
+// 6907380
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout <<"Hello, world!"<<endl
+    <<"I am ready for CS16!"<<endl;
+    return 0;
+}
